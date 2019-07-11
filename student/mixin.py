15d499221d9cbe9ddcc6f9a56c6ea3be5c2a25ed
funncy <gywns41@naver.com>
2019-07-12 02:15:44 +0900

    Added LoginRequiredMixin, StaffRequiredMixin

--- student/mixin.py
+++ student/mixin.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import redirect, reverse
+
 from .models import StudentInfo
 
 
+'''
+header.html 에 뿌려줄 학생 정보 데이터 가져오는 믹스인
+'''
 class StudentInfoMixin(object):
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -8,6 +15,9 @@ class StudentInfoMixin(object):
         context['student'] = StudentInfo.get_student(self.request.user.id)
         return context
 
+'''
+header.html 에 선택 메뉴 활성화를 위환 믹스인
+'''
 class ActiveMixin(object):
     active = None
 
@@ -15,3 +25,24 @@ class ActiveMixin(object):
         context = super().get_context_data(**kwargs)
         context[self.active] = True
         return context
+
+'''
+로그인한 상태인지 확인하는 믹스인
+'''
+class LoginRequiredMixin(object):
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+'''
+관리자인지 확인하는 믹스인
+'''
+class StaffRequiredMixin(object):
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        #관리자가 아닌경우 메인으로 리다이렉트
+        if self.request.user.is_staff is False:
+            return redirect(reverse('index'))
+        return super().dispatch(request, *args, **kwargs)
--- student/views.py
+++ student/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render, redirect, reverse
 from django.views.generic import ListView
 from university.models import University
 from student.models import StudentInfo
-from .mixin import ActiveMixin, StudentInfoMixin
+from .mixin import ActiveMixin, StudentInfoMixin, LoginRequiredMixin
 
 # Create your views here.
 
-class JoinLV(ActiveMixin, StudentInfoMixin, ListView):
+class JoinLV(ActiveMixin, LoginRequiredMixin, StudentInfoMixin, ListView):
     model = University
     template_name = 'join.html'
 
     def dispatch(self, request, *args, **kwargs):
+        #최초 로그인이 아니거나 로그인 상태가 아니면 메인 화면으로 리다이렉트
         studentInfo = StudentInfo.get_student(self.request.user.id)
-        if studentInfo is not None or self.request.user.id is None:
+        if studentInfo is not None:
             return redirect(reverse('index'))
         return super().dispatch(request, *args, **kwargs)
 
 
-class MypageLV(ActiveMixin, StudentInfoMixin, ListView):
+class MypageLV(ActiveMixin, LoginRequiredMixin, StudentInfoMixin, ListView):
     model = University
     template_name = 'mypage.html'
     active = 'mypageActive'
