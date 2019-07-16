3399203a5b86503a0eb7900dd8f5539f6205cea8
funncy <gywns41@naver.com>
2019-07-17 02:39:36 +0900

    Modified RuleSpecific rule_type choices

--- graduation/models.py
+++ graduation/models.py
@@ -18,17 +18,23 @@ class RuleGeneral(models.Model):
     name = models.CharField(verbose_name='졸업요건명', max_length=20)
     track = models.ForeignKey(Track, verbose_name='트랙', on_delete=models.CASCADE)
     year = models.SmallIntegerField(default=0)
-    type = models.IntegerField(verbose_name='졸업요건타입', choices=RULE_TYPE_CHOICES, default=1)
+    type = models.SmallIntegerField(verbose_name='졸업요건타입', choices=RULE_TYPE_CHOICES, default=1)
     value = models.SmallIntegerField(verbose_name='졸업요건값', default=0)
     subject_group = models.ForeignKey(SubjectGroup, verbose_name='과목그룹', on_delete=models.CASCADE)
 
 class RuleSpecific(models.Model):
+    RULE_TYPE_CHOICES = (
+        (1, '학점총합'),
+        (2, '과목갯수'),
+        (3, '영역갯수'),
+        (4, '학점평균'),
+    )
     university = models.ForeignKey(University, verbose_name='대학', on_delete=models.CASCADE)
     department = models.ForeignKey(Department, verbose_name='학과', on_delete=models.CASCADE)
     name = models.CharField(verbose_name='졸업요건명', max_length=20)
     track = models.ForeignKey(Track, verbose_name='트랙', on_delete=models.CASCADE)
     year = models.SmallIntegerField(default=0)
-    type = models.CharField(verbose_name='졸업요건타입', max_length=20)
+    type = models.SmallIntegerField(verbose_name='졸업요건타입', choices=RULE_TYPE_CHOICES, default=1)
     value = models.SmallIntegerField(verbose_name='졸업요건값', default=0)
     upper_rule = models.ForeignKey(RuleGeneral, verbose_name='상위졸업요건', on_delete=models.CASCADE, null=True, blank=True)
     subject_group = models.ForeignKey(SubjectGroup, verbose_name='과목그룹', on_delete=models.CASCADE)
